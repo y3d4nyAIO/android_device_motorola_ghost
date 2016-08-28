4c29cfbaee76942d68afacba781c6bd1bad7ba6b
klozz (TeamMEX) <xXx.Reptar.Rawrr.xXx@gmail.com>
2016-08-28 17:09:13 -0500

    ghost : Fix init extension build on N

--- init/init_ghost.cpp
+++ init/init_ghost.cpp
@@ -26,9 +26,10 @@
    OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN
    IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
+#include <unistd.h>
 #include <stdlib.h>
 
+#include <cutils/properties.h>
 #include "vendor_init.h"
 #include "property_service.h"
 #include "log.h"
@@ -56,7 +57,7 @@ static void set_cmdline_properties()
 
     for (i = 0; i < ARRAY_SIZE(prop_map); i++) {
         memset(prop, 0, PROP_VALUE_MAX);
-        rc = property_get(prop_map[i].src_prop, prop);
+        rc = property_get(prop_map[i].src_prop, prop, NULL);
         if (rc > 0) {
             property_set(prop_map[i].dest_prop, prop);
         } else {
@@ -100,15 +101,15 @@ void vendor_load_properties()
     char devicename[PROP_VALUE_MAX];
     int rc;
 
-    rc = property_get("ro.board.platform", platform);
+    rc = property_get("ro.board.platform", platform, NULL);
     if (!rc || !ISMATCH(platform, ANDROID_TARGET))
         return;
 
     set_cmdline_properties();
 
-    property_get("ro.boot.radio", radio);
-    property_get("ro.boot.carrier", carrier);
-    property_get("ro.boot.device", bootdevice);
+    property_get("ro.boot.radio", radio, NULL);
+    property_get("ro.boot.carrier", carrier, NULL);
+    property_get("ro.boot.device", bootdevice, NULL);
     if (ISMATCH(radio, "0x1")) {
         /* xt1058 */
         property_set("ro.product.device", "ghost_retail");
@@ -166,7 +167,7 @@ void vendor_load_properties()
         gsm_properties();
     }
 
-    property_get("ro.product.device", device);
+    property_get("ro.product.device", device, NULL);
     strlcpy(devicename, device, sizeof(devicename));
     INFO("Found device: %s radio id: %s carrier: %s Setting build properties for %s device\n", bootdevice, radio, carrier, devicename);
 }
