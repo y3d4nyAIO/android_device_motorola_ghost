f1a5b54ce4ac64f33db55ed6e8d0a4c8f2d20eaf
klozz <xxx.reptar.rawrr.xxx@gmail.com>
2016-08-28 11:53:14 -0500

    >:c
    Signed-off-by: klozz <xxx.reptar.rawrr.xxx@gmail.com>

--- xpe.mk
+++ xpe.mk
@@ -6,8 +6,8 @@ $(call inherit-product, vendor/XPe/config/common_full_phone.mk)
 # Enhanced NFC
 $(call inherit-product, vendor/XPe/config/nfc_enhanced.mk)
 
-PRODUCT_RELEASE_NAME := MOTO X
 PRODUCT_NAME := xpe_ghost
+PRODUCT_RELEASE_NAME := MOTO X
 PRODUCT_DEVICE := ghost
 PRODUCT_BRAND := Motorola
 PRODUCT_MANUFACTURER := Motorola
