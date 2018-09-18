fd184e1dd9163c93245d0e1a49fda09c73ba9193
y3d4nyAIO <yedany10@gmail.com>
2018-09-18 09:41:42 -0500

    Du changes

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -14,4 +14,4 @@
 # limitations under the License.
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_ghost.mk
+    $(LOCAL_DIR)/du_ghost.mk
--- du.mk
+++ du.mk
@@ -1,14 +1,11 @@
 $(call inherit-product, device/motorola/ghost/full_ghost.mk)
 
 # Inherit some common XPe stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/du/config/common_full_phone.mk)
 
 $(call inherit-product, device/motorola/msm8960dt-common/twrp.mk)
 
-# Enhanced NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
-
-PRODUCT_NAME := lineage_ghost
+PRODUCT_NAME := du_ghost
 PRODUCT_RELEASE_NAME := MOTO X
 PRODUCT_DEVICE := ghost
 PRODUCT_BRAND := Motorola
